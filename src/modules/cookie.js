617f01ad687abe04c39ded961f5220e1d0a2e715
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2021-09-19 19:37:22 -0300

    master

--- src/modules/cookie.js
+++ src/modules/cookie.js
@@ -1,29 +1,13 @@
+import Cookies from 'js-cookie'
 
-/*function setCookie(nome, valor) {
-    var validade = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        validade = "; expires=" + date.toUTCString();
-    }
-    document.cookie = nome + "=" + (valor || "") + validade + "; path=/";
+const myCookie = Cookies.get('gamePadreFernando');
+const acceptCookies = document.querySelector('.accept-cookies');
+
+if (myCookie) acceptCookies.remove('div')
+else {
+    const btnCookies = document.querySelector('.button-cookies');
+    btnCookies.addEventListener('click', () => {
+        Cookies.set('gamePadreFernando', 'value', { expires: 365 });
+        acceptCookies.remove('div');
+    })
 }
-
-function getCookie(nome) {
-    var nomeCookie = nome + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nomeCookie) == 0) return c.substring(nomeCookie.length, c.length);
-    }
-    return null;
-} */
-
-
-const div = document.querySelector('.acept-cookies');
-const button = document.querySelector('.button-cookies');
-
-button.addEventListener('click', e => {
-    if (e.target) div.remove('div');
-});
