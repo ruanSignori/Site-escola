adc4c844563fa23ca4ed10ab5e36a555f52f92e1
ruanSignori <cleosignori@gmail.com>
2021-10-16 13:48:09 -0300

    update

--- src/modules/script.js
+++ src/modules/script.js
@@ -3,7 +3,7 @@ import ScrollReveal from 'scrollreveal';
 const to_top = document.querySelector('.smooth-to-top');
 
 window.addEventListener('scroll', () => {
-    to_top.style.animation = "opacity 1s";
+    to_top.style.animation = "fadeVisibility 2s";
     to_top.style.opacity = 1;
 })
 
@@ -13,7 +13,6 @@ to_top.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
-
 
 ScrollReveal().reveal('video', { 
     delay: 300 
@@ -67,3 +66,47 @@ ScrollReveal().reveal('.smooth-animation', {
     easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
     delay: 30
 });
+
+
+class Carousel {
+    constructor(dataOfslides, classNextButton, classPreviousButton, classAddedAndRemoved) {
+      this.slides = document.querySelectorAll(dataOfslides);
+      this.lastSlideIndex = this.slides.length - 1;
+      this.currentSlideIndex = 0;
+      this.classAdd_Remove = classAddedAndRemoved;
+  
+      this.nextButton = document.querySelector(classNextButton);
+      this.previousButton = document.querySelector(classPreviousButton);
+  
+      this.events();
+    }
+  
+    events() {
+      this.nextButtonEvent();
+      this.previousButtonEvent();
+    }
+  
+  
+    nextButtonEvent() {
+      this.nextButton.addEventListener('click', () => {
+        const correctSlideIndex = this.currentSlideIndex === this.lastSlideIndex ? this.currentSlideIndex = 0 : ++this.currentSlideIndex;
+        this.switchingClassOfSlides(correctSlideIndex);
+      });
+    }
+  
+    previousButtonEvent() {
+      this.previousButton.addEventListener('click', () => {
+        const correctSlideIndex = this.currentSlideIndex === 0 ? this.currentSlideIndex = this.lastSlideIndex : --this.currentSlideIndex;
+        this.switchingClassOfSlides(correctSlideIndex);
+      });
+    }
+  
+    switchingClassOfSlides(correctSlideIndex) {
+      this.slides.forEach(e => e.classList.remove(this.classAdd_Remove));
+      this.slides[correctSlideIndex].classList.add(this.classAdd_Remove);
+    }
+  
+}
+//Se criar um novo carousel apenas adicionar um numero a mais, no html, js e css.
+const slide_1 = new Carousel('[data-js="carousel__item"]', '[data-js="carousel__button--next"]', '[data-js="carousel__button--prev"]', 'carousel__item--visible');
+const slide_2 = new Carousel('[data-js="carousel__item2"]', '[data-js="carousel__button--next2"]', '[data-js="carousel__button--prev2"]', 'carousel__item--visible2');
