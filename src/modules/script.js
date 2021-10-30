8f48bd771eaf9ced6e1049713434a7f006f1a2d5
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2021-10-30 15:57:01 -0300

    update

--- src/modules/script.js
+++ src/modules/script.js
@@ -3,8 +3,8 @@ import ScrollReveal from 'scrollreveal';
 const to_top = document.querySelector('.smooth-to-top');
 
 window.addEventListener('scroll', () => {
-    to_top.style.animation = "fadeVisibility 2s";
     to_top.style.opacity = 1;
+    to_top.style.animation = "fadeVisibilityRight 0.7s";
 })
 
 to_top.addEventListener('click', () => {
