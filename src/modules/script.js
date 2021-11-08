d7423600e30cd7e19830335576bd10a6072458ab
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2021-11-08 10:51:17 -0300

    Correção de bugs e ajustes

--- src/modules/script.js
+++ src/modules/script.js
@@ -5,63 +5,72 @@ import ScrollReveal from 'scrollreveal';
 const to_top = document.querySelector('.smooth-to-top');
 
 window.addEventListener('scroll', () => {
-    to_top.style.opacity = 1;
-    to_top.style.animation = "fadeVisibilityRight 0.7s";
+  to_top.style.opacity = 1;
+  to_top.style.animation = "fadeVisibilityRight 0.7s";
 })
 
 to_top.addEventListener('click', () => {
-    window.scroll({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scroll({
+      top: 0,
+      behavior: 'smooth'
+  });
 });
 
 //Todos as animações de scroll do site
 ScrollReveal().reveal('.paragraph', { 
-    delay: 80, 
-    reset: true,
-    origin: 'bottom',
-    distance: '50px',
+  delay: 80, 
+  reset: true,
+  origin: 'bottom',
+  distance: '50px',
+  interval: 100
 });
 
 ScrollReveal().reveal('.span-animated', {
-    reset: true,
-    distance: '150px',
-    origin: 'bottom',
-    duration: 1000,
-    easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
-    delay: 30
+  reset: true,
+  distance: '150px',
+  origin: 'bottom',
+  duration: 1000,
+  easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
+  delay: 30
 });
 
 ScrollReveal().reveal('.span-animated-school', {
-    reset: true,
-    distance: '150px',
-    origin: 'bottom',
-    duration: 1400,
-    easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
-    delay: 30
+  reset: true,
+  distance: '150px',
+  origin: 'bottom',
+  duration: 1400,
+  easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
+  delay: 30
 });
 
 ScrollReveal().reveal('.accept-cookies', {
-    delay: 2000,
-    distance: '150px',
-    origin: 'bottom'
+  delay: 2000,
+  distance: '150px',
+  origin: 'bottom'
 });
 
 ScrollReveal().reveal('.smooth-animation', {
-    distance: '170px',
-    origin: 'bottom',
-    duration: 1500,
-    easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
-    delay: 30
+  distance: '170px',
+  origin: 'bottom',
+  duration: 1500,
+  easing: 'cubic-bezier(0.73, 0.12, 0.35, 0.96)',
+  delay: 30
 });
 
-//Mundaça de imagem automática na parte da histório do jogo
-/*setInterval(() => {
-  const img = document.querySelector('.alternate-img-history');
-  img.src = './src/assets/img/history/imagem-2.webp';
-  img.classList.add('animation-history')
-}, 3000) */
+ScrollReveal().reveal('.item a button', {
+  scale: 0.40,
+  duration: 600,
+  reset: true
+})
+
+ScrollReveal().reveal('.content-child-3', {
+  reset: true,
+  scale: 0.80,
+  delay: 50,
+  duration: 700
+});
+
+
 
 //Script para o funcionamento do carousel
 class Carousel {
@@ -74,7 +83,7 @@ class Carousel {
       this.nextButton = document.querySelector(classNextButton);
       this.previousButton = document.querySelector(classPreviousButton);
 
-      this.classLengthSlide = document.querySelector('.length-carousel-image');
+      this.classLengthSlide = document.querySelector('.txt-length');
   
       this.events();
     }
@@ -82,20 +91,13 @@ class Carousel {
     events() {
       this.nextButtonEvent();
       this.previousButtonEvent();
-      this.pushLength();
-    }
-  
-  
-    pushLength () {
-      if(this.nextButtonEvent) {
-        
-      }
     }
 
     nextButtonEvent() {
       this.nextButton.addEventListener('click', () => {
         const correctSlideIndex = this.currentSlideIndex === this.lastSlideIndex ? this.currentSlideIndex = 0 : ++this.currentSlideIndex;
-        console.log(this.currentSlideIndex)
+
+        this.classLengthSlide.innerHTML = `${this.currentSlideIndex}/${this.slides.length} `
         this.switchingClassOfSlides(correctSlideIndex);
 
         const addAnimation = document.querySelector(`.${this.classAdd_Remove}`);
@@ -106,8 +108,15 @@ class Carousel {
     previousButtonEvent() {
       this.previousButton.addEventListener('click', () => {
         const correctSlideIndex = this.currentSlideIndex === 0 ? this.currentSlideIndex = this.lastSlideIndex : --this.currentSlideIndex;
-        this.switchingClassOfSlides(correctSlideIndex);
+
         
+        if (this.lastSlideIndex === 0) {
+          this.classLengthSlide.innerHTML = `${this.currentSlideIndex + 1}/${this.slides.length} `
+        } else {
+          this.classLengthSlide.innerHTML = `${this.currentSlideIndex + 1}/${this.slides.length} `
+        }
+
+        this.switchingClassOfSlides(correctSlideIndex);
 
         const addAnimation = document.querySelector(`.${this.classAdd_Remove}`);
         addAnimation.style.animation = 'fadeVisibilityLeft 0.5s';
