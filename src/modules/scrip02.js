40f1037f93e51be64d96be2f8e68e6434b100e7b
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2021-11-15 11:08:15 -0300

    event download test

--- src/modules/scrip02.js
+++ src/modules/scrip02.js
@@ -1,9 +1,16 @@
 const downloadLink = document.querySelector('.download');
 
-downloadLink.addEventListener('click', () => {
-    downloadLink.setAttribute('href', 'https://download855.mediafire.com/p4o41hqa1s4g/wp2wdog1gyklajj/MageSiel.rar');
-    downloadLink.setAttribute('download', 'true');
+const to_top = document.querySelector('.smooth-to-top');
+
+window.addEventListener('scroll', () => {
+  to_top.style.opacity = 1;
+  to_top.style.animation = "fadeVisibilityRight 0.7s";
+})
+
+to_top.addEventListener('click', () => {
+  window.scroll({
+      top: 0,
+      behavior: 'smooth'
+  });
 });
 
-downloadLink.removeAttribute('href');
-downloadLink.removeAttribute('download')
