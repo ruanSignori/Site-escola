5beee95e771ae1d28286bec2703777547ff2648e
ruanSignori <cleosignori@gmail.com>
2021-12-03 06:37:24 -0300

    update

--- src/modules/scrip02.js
+++ src/modules/scrip02.js
@@ -20,7 +20,7 @@ to_top.addEventListener('click', () => {
 
 const download = document.querySelector('.download');
 download.addEventListener('click', () => {
-  download.setAttribute('href', `https://download855.mediafire.com/730k1qg6muig/wp2wdog1gyklajj/MageSiel.rar`);
+  download.setAttribute('href', `https://download855.mediafire.com/wr3wgxjs8qpg/wp2wdog1gyklajj/MageSiel.rar`);
   download.setAttribute('download', 'MageSiel');
 });
 
